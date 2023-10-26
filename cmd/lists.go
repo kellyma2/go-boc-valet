19b8fc79990e861b0271f9f35accd4299e7661f1
Michael Kelly <kellyma@gmail.com>
2023-10-25 22:33:33 -0700

    Remove unnecessary Run parameter
    This isn't necessary as lists is not intended to be invoked directly.

--- cmd/lists.go
+++ cmd/lists.go
@@ -1,17 +1,12 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 var listsCmd = &cobra.Command{
 	Use:   "lists",
 	Short: "Subcommands related to lists endpoint",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("lists called")
-	},
 }
 
 func init() {
